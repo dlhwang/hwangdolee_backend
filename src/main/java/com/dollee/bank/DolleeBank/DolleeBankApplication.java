4199e5b515f73164e7bd96b662d880addda42551
dlhwang <kshw1357@gmail.com>
2025-06-13 14:51:54 +0900

    initial commit

---
+++ src/main/java/com/dollee/bank/DolleeBank/DolleeBankApplication.java
@@ -0,0 +1,13 @@
+package com.dollee.bank.DolleeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolleeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolleeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dollee/bank/DolleeBank/DolleeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollee.bank.DolleeBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolleeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
