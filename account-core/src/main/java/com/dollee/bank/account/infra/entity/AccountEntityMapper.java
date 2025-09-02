aae048edde7bca6011f84d525835ad6c6bb21f95
dlhwang <dlhwang@ackertonts.com>
2025-09-02 16:25:18 +0900

    수정

--- account-core/src/main/java/com/dollee/bank/account/infra/entity/AccountEntityMapper.java
+++ account-core/src/main/java/com/dollee/bank/account/infra/entity/AccountEntityMapper.java
@@ -5,29 +5,30 @@ import com.dollee.bank.account.domain.model.AccountNumber;
 import com.dollee.bank.common.util.Money;
 
 public class AccountEntityMapper {
-  public static AccountEntity toEntity(Account domain) {
-    return new AccountEntity(
-        domain.getAccountId(),
-        domain.getAccountNumberToString(),
-        domain.getAmount(),
-        domain.getUserId()
-    );
-  }
 
-  public static AccountEntity toEntityForSave(Account domain) {
-    return new AccountEntity(
-        domain.getAccountNumberToString(),
-        domain.getAmount(),
-        domain.getUserId()
-    );
-  }
+    public static AccountEntity toEntity(Account domain) {
+        return new AccountEntity(
+            domain.getAccountId(),
+            domain.getAccountNumberToString(),
+            domain.getAmount(),
+            domain.getUserId()
+        );
+    }
 
-  public static Account toDomain(AccountEntity entity) {
-    return Account.of(
-        entity.getId(),
-        AccountNumber.to(entity.getAccountNumber()),
-        Money.wons(entity.getBalance()),
-        entity.getUserId()
-    );
-  }
+    public static AccountEntity toEntityForSave(Account domain) {
+        return new AccountEntity(
+            domain.getAccountNumberToString(),
+            domain.getAmount(),
+            domain.getUserId()
+        );
+    }
+
+    public static Account toDomain(AccountEntity entity) {
+        return Account.of(
+            entity.getId(),
+            AccountNumber.to(entity.getAccountNumber()),
+            Money.wons(entity.getBalance()),
+            entity.getUserId()
+        );
+    }
 }
